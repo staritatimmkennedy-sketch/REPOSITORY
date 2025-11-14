b028cd97e2a8dfd40adfd1212efa2505d0c4c426
staritatimmkennedy-sketch <staritatimmkennedy@nddu.edu.ph>
2025-11-14 19:41:27 +0800

    Update db.php

--- ethanTwo/db.php
+++ ethanTwo/db.php
@@ -1,10 +1,10 @@
 <?php
-$db_host = 'localhost';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'library_repository_db';
-$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
-
+$db_host = getenv('mysql.railway.internal') ?: 'localhost';
+$db_username = getenv('root') ?: 'root';
+$db_password = getenv('zHoqJftiBratOikAHUOapfORUUBiHDFd') ?: '';
+$db_name = getenv('railway') ?: 'library_repository_db';
+$db_port = getenv('3306') ?: '3306';
+$dsn = "mysql:host=$db_host;port=$db_port;dbname=$db_name;charset=utf8mb4";
 $conn = null;
 
 try {
@@ -14,6 +14,10 @@ try {
     ]);
 } catch (PDOException $e) {
     error_log("DB Connection failed: " . $e->getMessage());
-    $conn = null;
+    if (getenv('RAILWAY_ENVIRONMENT')) {
+        die("Database connection error. Please try again later.");
+    } else {
+        die("DB Connection failed: " . $e->getMessage());
+    }
 }
 ?>
